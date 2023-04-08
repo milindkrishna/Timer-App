8bc730020b70aff2678c53a0721d80dd02c1a659
Milind Krishna <54195987+milindkrishna@users.noreply.github.com>
2023-04-08 11:25:59 +0530

    Added Load Spinner
    Code Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   const [time,settime] = useState('')
   const [gmttime,setgmttime] = useState('')
   const [esttime,setesttime] = useState('')
-
+  const [loading, setloading] = useState(true)
   useEffect(() => {
 
     // componentDidMount
@@ -18,12 +18,25 @@ export default function App() {
       setgmttime(now.toLocaleString('en-US', {weekday: 'short', day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true, timeZone: 'GMT'}))
       
       setesttime(now.toLocaleDateString('en-US',{ weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' })+' '+now.toLocaleTimeString('en-US',{timeZone:'America/New_York'}))
+    
+    setloading(false)
+    
     },1000)
+
+    
 
     // componentWillUnmount
     return () => clearInterval(interval);
   
   },[])
+
+  if(loading){
+    return (
+      <div className='loader'>
+        <div className='loader-spinner'></div>
+      </div>
+    );
+  }
   
   return (
     <div className='time-container'>
